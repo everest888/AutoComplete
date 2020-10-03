2a808224a12378f8d86a51a6772066e1880d19ff
everest888 <67152246+everest888@users.noreply.github.com>
2020-10-03 12:22:41 +0600

    Add files via upload

---
+++ style.js
@@ -0,0 +1,40 @@
+
+
+new Vue({
+    data() {
+        return {
+            currentFocus: '',
+            autocomplete: '',
+            onBlur: true,
+            onFocus: false,
+            arr: ["Смеси", "Черепица", "Линолеум","Гипсокартонный лист", "Профиль направляющий", "Паркетная доска"]
+        }
+    },
+    mounted() {
+        var vm = this;
+        document.addEventListener("click", function (e) {
+
+            vm.onBlur ? vm.onFocus = false : false
+        });
+
+    },
+    methods: {
+        addActive() {
+            var vm = this;
+            if (!vm.array) return false;
+            if (vm.currentFocus >= vm.array.length) vm.currentFocus = 0;
+            if (vm.currentFocus < 0) vm.currentFocus = (vm.array.length - 1);
+        },
+        keyDown(e) {
+            var vm = this;
+            if (e.keyCode == 40) {
+                vm.currentFocus++;
+                vm.addActive()
+            } else if (e.keyCode == 38) {
+                vm.currentFocus--;
+                vm.addActive()
+
+            }
+        }
+    }
+}).$mount('#app')
